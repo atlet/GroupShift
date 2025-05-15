2a2379199287508259ddd08085763266837583cf
Martin Božič <martin.bozic@arnes.si>
2025-05-15 11:47:58 +0200

    new localization

---
+++ lang/sl/local_groupshift.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/**
+ * @package  local_groupshift
+ * @copyright 2024, Andraž Prinčič <atletek@gmail.com>
+ * @copyright 2024, Martin Božič <martin.bozic@arnes.si>
+ * @license MIT
+ * @doc https://docs.moodle.org/dev/String_API
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Premikanje po skupinah';
+$string['plugintitle'] = 'Vtičnik za premikanje udeležencev iz ene skupin v drugo znotraj predmeta glede na nastavljene pogoje.';
+$string['menuentry'] = 'Premikanje po skupinah';
+// local/groupshift/index.php
+$string['filtertype'] = 'Pogoj';
+$string['badgeachieved'] = 'Osvojena značka';
+$string['badgenotachieved'] = 'Značka ni osvojena';
+$string['subjectcompleted'] = 'Predmet zaključen';
+$string['subjectnocompleted'] = 'Predmet ni bil zaključen';
+$string['badges'] = 'Značke';
+$string['help'] = 'Izberite pogoje in kliknite "Shrani spremembe" za nadaljevanje na naslednjo stran. Tam boste videli skupine s številom udeležencev glede na izbrani pogoj.';
+// local/groupshift/manage.php
+$string['notingroup'] = 'Ni v skupini.';
+$string['managehelp'] = 'Premaknite uporabnike iz ene skupine v drugo.';
+$string['fromgroup'] = 'Iz skupine';
+$string['togroup'] = 'V skupino';
+$string['selectdifferentgroups'] = 'Izbrati morate različni skupini!';
+$string['sucesfullymoved'] = 'Vsi uporabniki so bili uspešno premaknjeni.';
+$string['selectedcondition'] = 'Izbrani pogoj';
