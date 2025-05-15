cb62fbcb8139b90861a4dece74fe318b8a5c6071
Martin Božič <martin.bozic@arnes.si>
2025-05-15 15:20:24 +0200

    Update local_groupshift.php
    letnica in komentarji

--- lang/sl/local_groupshift.php
+++ lang/sl/local_groupshift.php
@@ -4,7 +4,7 @@
 /**
  * @package  local_groupshift
  * @copyright 2024, Andraž Prinčič <atletek@gmail.com>
- * @copyright 2024, Martin Božič <martin.bozic@arnes.si>
+ * @copyright 2025, Martin Božič <martin.bozic@arnes.si>
  * @license MIT
  * @doc https://docs.moodle.org/dev/String_API
  */
@@ -14,7 +14,6 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Premikanje po skupinah';
 $string['plugintitle'] = 'Vtičnik za premikanje udeležencev iz ene skupin v drugo znotraj predmeta glede na nastavljene pogoje.';
 $string['menuentry'] = 'Premikanje po skupinah';
-// local/groupshift/index.php
 $string['filtertype'] = 'Pogoj';
 $string['badgeachieved'] = 'Osvojena značka';
 $string['badgenotachieved'] = 'Značka ni osvojena';
@@ -22,7 +21,6 @@ $string['subjectcompleted'] = 'Predmet zaključen';
 $string['subjectnocompleted'] = 'Predmet ni bil zaključen';
 $string['badges'] = 'Značke';
 $string['help'] = 'Izberite pogoje in kliknite "Shrani spremembe" za nadaljevanje na naslednjo stran. Tam boste videli skupine s številom udeležencev glede na izbrani pogoj.';
-// local/groupshift/manage.php
 $string['notingroup'] = 'Ni v skupini.';
 $string['managehelp'] = 'Premaknite uporabnike iz ene skupine v drugo.';
 $string['fromgroup'] = 'Iz skupine';
